011c7bb0b04f258979463407b78dac258376b343
mangqiqi <836778763@qq.com>
2017-09-13 22:10:58 +0800

    Add files via upload

---
+++ Code/main.cpp
@@ -0,0 +1,28 @@
+#include "sudoku.h"
+
+int main(int argc, char *argv[])
+{
+  	sudoku[0][0]=5;
+	FILE* fp;
+
+  if(argc == 1 || argc == 2 || argc > 3)
+  {
+    cout<<"输入非法!\n";
+  }
+  else
+  {
+    string string1 = argv[1];
+    string string2 = argv[2];
+    if(string1 != "-c" || !stringtonum(string2))
+    {
+      cout<<"输入非法!\n";
+    }
+    else
+    {
+      	fp = fopen("sudoku.txt","w");
+    	backtrace(0,fp);
+    	fclose(fp);
+    }
+  }
+  return 0;
+}
